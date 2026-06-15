8b69a7b8376d9575ae8d04741bb4e9978375dd42
aqib-mulla <40908331+aqib-mulla@users.noreply.github.com>
2026-06-15 20:17:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -88,9 +88,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 // Allow requests from the frontend origin (http://localhost:3000)
 app.use(cors({ origin: 'http://localhost:3000' }));
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+// app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-
+app.use(express.static(path.join(__dirname, './build')));
 //FILE  STORAGE CONFIG
 // const storage = multer.diskStorage({
 //     destination:function(req, file, cb){
@@ -451,6 +451,9 @@ app.get("/auth/get-pdf/:filename", (req, res) => {
 });
 
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname,'./build/index.html'))
+})
 
 
 
